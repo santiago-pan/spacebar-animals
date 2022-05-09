2b62ce2a33dae1f494ba770ede970d75965d4618
s-pan <pan@qelp.com>
2022-05-09 12:32:36 +0200

    First comit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const counter = useKeyCounter();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-section">
+        <ImageComponent index={counter} />
+      </section>
     </div>
   );
+}
+
+function ImageComponent(props: { index: number }) {
+  return <img alt="animal" src={`/images/nature/${props.index}img.png`} />;
+}
+
+function useKeyCounter() {
+  const [counter, setCounter] = useState<number>(1);
+  useEffect(() => {
+    const upHandler = (evt: KeyboardEvent) => {
+      if (evt.key === " ") {
+        setCounter((cnt) => (cnt === 109 ? 1 : cnt + 1));
+      }
+      if (evt.key === "ArrowLeft") {
+        setCounter((cnt) => (cnt > 1 ? cnt - 1 : cnt));
+      }
+    };
+    const touchendHandler = (evt: TouchEvent) => {
+      setCounter((cnt) => (cnt === 109 ? 1 : cnt + 1));
+    };
+    window.addEventListener("keyup", upHandler);
+    window.addEventListener("touchend", touchendHandler);
+    return () => {
+      window.removeEventListener("keyup", upHandler);
+      window.removeEventListener("touchend", touchendHandler);
+    };
+  }, []);
+  return counter;
 }
 
 export default App;
